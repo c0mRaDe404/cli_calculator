806e7a1a6fdc53d5b2995a31242b9b32c4b36864
c0mRaDe404 <bhuvantk160@gmail.com>
2023-12-19 01:26:46 +0530

    minor update

--- src/calc.c
+++ src/calc.c
@@ -23,25 +23,39 @@ int evaluate(char expression[])
 {
     operand.top = operator.top = -1;
 
-    push(&operand,20);
+   
 
-
-    printf("data is %d and postion is %d",operand.data[0],operand.top);
-
-
-
-    for(int i = 0;expression[i] != '\0';i++)
+    for(int i = 0;expression[i] != '\0';)
     {
-        int operand = 0;
+        int Operand = 0;
 
-        if(isDigit(expression[i]))
-        {
-            operand = operand*10+(expression[i]-'0');
-        }
         
-    }
+        while(isDigit(expression[i]))
+        {
+            Operand = Operand*10+(expression[i]-'0');
+            i++;
+        }
+
+        push(&operand,Operand);
+        
+
+         if (isSymbol(expression[i]))
+        {
+            push(&operator,expression[i]);
+            i++;
+        }
+   
+        
+    } // << Loop ends here
+
+
+    printf("%d\n",operand.data[0]);
+      printf("%d\n",operand.data[1]);
+        printf("%c\n",operator.data[0]);
+          printf("%c\n",operator.data[1]);
 
 }
+
 
 
 
@@ -51,4 +65,19 @@ int isDigit(char value)
     if(value >= '0' && value <= '9')
         return 1;
     else return 0;
+}
+
+
+int isSymbol(char symbol)
+{
+    if(symbol >='('|| symbol <= '/' ) return 1;
+    else return 0;
+
+}
+
+int checkPriority(char symbol)
+{
+    if(symbol == '+' || symbol == '-') return 0;
+    if(symbol == '*' || symbol == '/') return 1;
+    if(symbol == '(' || symbol == ')') return 2;
 }
